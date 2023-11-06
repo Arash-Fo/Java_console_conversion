35bec33a070bfe5a5c2777b7560a09db34f2c9fe
Arash-Fo <129895130+Arash-Fo@users.noreply.github.com>
2023-11-06 12:19:16 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java_console_conversion
+ 
---
+++ src/main/java/com/mycompany/java_console_conversion/Java_console_conversion.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.java_console_conversion;
+
+/**
+ *
+ * @author Arash
+ */
+public class Java_console_conversion {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
